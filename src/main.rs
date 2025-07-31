7c8535fdebb949176937db895d454ec40ad3355d
Melisa <melisadmendizabal@gmail.com>
2025-07-30 23:55:50 -0600

    main render loop

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ mod framebuffer;
 use crate::framebuffer::Framebuffer;
 mod line;
 use line::line;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let window_width = 800;
@@ -21,23 +23,62 @@ fn main() {
     let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height, Color::new(50,50,100,255));
 
     framebuffer.set_background_color(Color::new(50,50,100,255));
-    framebuffer.clear();
 
-    framebuffer.set_curret_color(Color::GREEN);
-    line (
-        &mut framebuffer,
-        Vector2::new(50.0, 50.0),
-        Vector2::new(350.0, 350.0),
-    );
+    let mut translate_x = 0.0;
+    let mut translate_y = 0.0;
 
-    framebuffer.set_curret_color(Color::RED);
-    line (
-        &mut framebuffer,
-        Vector2::new(350.0, 50.0),
-        Vector2::new(50.0, 350.0),
-    );
+    let mut direction_x = 1.0;
+    let mut direction_y = 1.0;
+    let speed = 2.0;
 
     while !window.window_should_close() {
-        framebuffer.swap_buffers(&mut window, &raylib_thread)
+        if translate_x + 50.0 > framebuffer_width as f32 || translate_x < 0.0 {
+            direction_x = - direction_x;
+        }
+
+        if translate_y + 50.0 > framebuffer_height as f32 || translate_y < 0.0 {
+            direction_y = - direction_y;
+        }
+
+        translate_x += direction_x * speed;
+        translate_y += direction_y * speed;
+      
+
+        framebuffer.clear();
+
+        render(&mut framebuffer, translate_x, translate_y);
+
+        // Si se presiona F12, hacer un screenshot
+        if window.is_key_pressed(KeyboardKey::KEY_F12) {
+            framebuffer.render_to_file("screenshot.png");
+            println!("Screenshot guardado como screenshot.png");
+        }
+
+
+
+        framebuffer.swap_buffers(&mut window, &raylib_thread);
+
+        thread::sleep(Duration::from_millis(16));
+
     }
+}
+
+fn render(
+    framebuffer:&mut Framebuffer,
+    translate_x: f32,
+    translate_y: f32,
+) {
+    framebuffer.set_background_color(Color::GREEN);
+    line(
+        framebuffer,
+        Vector2::new(50.0 + translate_x, 50.0 +translate_y),
+        Vector2::new(350.0 + translate_x, 350.0 +translate_y),
+    );
+
+    framebuffer.set_background_color(Color::RED);
+    line(
+        framebuffer,
+        Vector2::new(350.0 + translate_x, 50.0 +translate_y),
+        Vector2::new(50.0 + translate_x, 350.0 +translate_y),
+    );
 }
